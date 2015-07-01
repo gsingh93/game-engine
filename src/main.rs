8237e2582334f37e26f054abd9258351ac927c22
Gulshan Singh <gsingh2011@gmail.com>
2015-07-01 11:20:20 -0400

    Refactored Scene and EngineContext

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,33 @@ use draw::{Cube, Grid, GameObject};
 use shader::ShaderType;
 
 use glium::{glutin, Display, DisplayBuild, DrawError, Program, Surface};
-use glium::backend::Facade;
 use glium::glutin::{ElementState, VirtualKeyCode};
 
 use nalgebra::{zero, BaseFloat, Vec3};
 
-type Scene = Vec<Box<GameObject>>;
+struct Scene {
+    objects: Vec<Box<GameObject>>,
+    camera: Camera,
+}
+
+impl Scene {
+    fn new(camera: Camera) -> Self {
+        Scene { camera: camera, objects: Vec::new() }
+    }
+
+    fn draw(&self, ctxt: &mut EngineContext) {
+        let mut target = ctxt.display.draw();
+        target.clear_color_and_depth((0., 0., 0., 1.), 1.);
+        for obj in self.objects.iter() {
+            ctxt.draw(&mut target, &self.camera, obj).unwrap();
+        }
+        target.finish().unwrap();
+    }
+
+    fn add<G: GameObject + 'static>(&mut self, object: G) {
+        self.objects.push(Box::new(object));
+    }
+}
 
 const RELATIVE_ROTATION: bool = true;
 
@@ -41,30 +62,26 @@ fn get_display_dim(display: &Display) -> (u32, u32) {
 }
 
 pub struct EngineContext {
+    display: Display,
     vertex_shader: String,
     shader_map: HashMap<ShaderType, String>,
 }
 
 impl EngineContext {
-    pub fn new() -> Self {
+    pub fn new(display: Display) -> Self {
         let mut shader = String::new();
         File::open("shaders/vertex.glsl").unwrap().read_to_string(&mut shader).unwrap();
-        EngineContext { vertex_shader: shader, shader_map: HashMap::new() }
+        EngineContext { display: display, vertex_shader: shader, shader_map: HashMap::new() }
     }
 
-    pub fn draw<'a, F: Facade, S: Surface>(&mut self,
-                                           surface: &mut S,
-                                           facade: &F,
-                                           camera: &Camera,
-                                           obj: &Box<GameObject>)
-                                           -> Result<(), DrawError> {
+    pub fn draw<S: Surface>(&mut self, surface: &mut S, camera: &Camera,
+                            obj: &Box<GameObject>) -> Result<(), DrawError> {
         let parent = obj.parent();
         let uniforms = obj.construct_uniforms(&camera);
 
-        let &mut EngineContext { ref vertex_shader, ref mut shader_map } = self;
+        let &mut EngineContext { ref display, ref vertex_shader, ref mut shader_map } = self;
         let fragment_shader = Self::get_shader(shader_map, parent.shader_type);
-        let program = Program::from_source(facade, vertex_shader,
-                                           fragment_shader, None).unwrap();
+        let program = Program::from_source(display, vertex_shader, fragment_shader, None).unwrap();
         surface.draw(&parent.vertex_buffer, parent.indices.clone(), &program, &uniforms,
                      &parent.draw_params)
     }
@@ -88,29 +105,24 @@ fn main() {
         .unwrap();
 
     let mut camera_pos = Vec3::new(0., 0., 1.);
-    let mut camera = {
+    let camera = {
         let (w, h) = get_display_dim(&display);
         let (w, h) = (w as f32, h as f32);
         Camera::new(camera_pos.clone(), w / h)
     };
 
-    let mut scene = Scene::new();
-    scene.push(Box::new(Grid::new(&display, 20)));
-    scene.push(Box::new(Cube::new(&display, 0.25, zero())));
+    let mut scene = Scene::new(camera);
+    scene.add(Grid::new(&display, 20));
+    scene.add(Cube::new(&display, 0.25, zero()));
 
     let mut mouse_pressed = false;
     let mut old_mouse_coords = None;
 
-    let mut ctxt = EngineContext::new();
+    let mut ctxt = EngineContext::new(display);
     loop {
-        let mut target = display.draw();
-        target.clear_color_and_depth((0., 0., 0., 1.), 1.);
-        for obj in scene.iter() {
-            ctxt.draw(&mut target, &display, &camera, obj).unwrap();
-        }
-        target.finish().unwrap();
+        scene.draw(&mut ctxt);
 
-        for ev in display.poll_events() {
+        for ev in ctxt.display.poll_events() {
             match ev {
                 glutin::Event::KeyboardInput(ElementState::Pressed, _, Some(key)) => {
                     match key {
@@ -120,27 +132,27 @@ fn main() {
                         VirtualKeyCode::Right => camera_pos.x += 0.05,
                         VirtualKeyCode::R => {
                             camera_pos = Vec3::new(0., 0., 1.);
-                            camera.set_abs_rotation(0., 0.);
-                            camera.set_fov(BaseFloat::frac_pi_2());
+                            scene.camera.set_abs_rotation(0., 0.);
+                            scene.camera.set_fov(BaseFloat::frac_pi_2());
                         }
                         _ => ()
                     }
-                    camera.set_pos(&camera_pos);
+                    scene.camera.set_pos(&camera_pos);
                 },
                 glutin::Event::MouseWheel(glutin::MouseScrollDelta::LineDelta(_, v)) => {
-                    let fov = camera.fov();
+                    let fov = scene.camera.fov();
                     let new_fov = nalgebra::clamp(fov + (v * 0.05), 0., BaseFloat::pi());
-                    camera.set_fov(new_fov);
+                    scene.camera.set_fov(new_fov);
                 },
                 glutin::Event::MouseMoved((x, y)) => {
                     if mouse_pressed {
                         let (x, y) = (x as f32, y as f32);
-                        let (w, h) = get_display_dim(&display);
+                        let (w, h) = get_display_dim(&ctxt.display);
                         let (w, h) = (w as f32, h as f32);
                         if !RELATIVE_ROTATION {
                             let pitch = (y / h) * f32::two_pi();
                             let yaw = (x / w) * f32::two_pi();
-                            camera.set_abs_rotation(pitch, -yaw);
+                            scene.camera.set_abs_rotation(pitch, -yaw);
                         } else {
                             if let Some((x_old, y_old)) = old_mouse_coords {
                                 let delta_x = x - x_old;
@@ -148,7 +160,7 @@ fn main() {
 
                                 let pitch = (delta_y * 0.5 / h) * f32::two_pi();
                                 let yaw = (delta_x * 0.5 / w) * f32::two_pi();
-                                camera.rotate(pitch, yaw);
+                                scene.camera.rotate(pitch, yaw);
                             }
                             old_mouse_coords = Some((x, y));
                         }
@@ -163,7 +175,7 @@ fn main() {
                     };
                 },
                 glutin::Event::Resized(x, y) => {
-                    camera.set_aspect_ratio(x as f32 / y as f32);
+                    scene.camera.set_aspect_ratio(x as f32 / y as f32);
                 },
                 glutin::Event::Closed => return,
                 _ => ()
